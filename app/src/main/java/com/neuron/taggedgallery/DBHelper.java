c87506604441d149ce3e2b39357f4d64070b91c7
Timofey <yurev.timofey@gmail.com>
2020-09-14 03:25:32 +0300

    ВНЕДРЕНИЕ БАЗЫ ДАННЫХ НАЧАЛОСЬ.

---
+++ app/src/main/java/com/neuron/taggedgallery/DBHelper.java
@@ -0,0 +1,26 @@
+package com.neuron.taggedgallery;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int DB_VERSION = 1;
+
+    public DBHelper(@Nullable Context context, @Nullable String name, int version) {
+        super(context, name, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/neuron/taggedgallery/MainActivity.java
+++ app/src/main/java/com/neuron/taggedgallery/MainActivity.java
@@ -2,13 +2,30 @@ package com.neuron.taggedgallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 }
